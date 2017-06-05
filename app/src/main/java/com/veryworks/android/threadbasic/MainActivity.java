70878d3c08db40a5cddfc80b5f0b967794cfa55f
javafa <nigne2016@gmail.com>
2017-06-05 14:50:09 +0900

    init

--- app/src/main/java/com/veryworks/android/threadbasic/MainActivity.java
+++ app/src/main/java/com/veryworks/android/threadbasic/MainActivity.java
@@ -1,60 +1,33 @@
 package com.veryworks.android.threadbasic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 1.1 Thread 생성
-        Thread thread = new Thread(){
-            @Override
-            public void run() {
-                Log.i("Thread Test","Hello Thread!");
-            }
-        };
-
-        // 1.2. Thread 실행
-        thread.start(); // run() 함수를 실행시켜준다.
-
-        // 2.1. Thread 생성 2
-        Runnable thread2 = new Runnable() {
-            @Override
-            public void run() {
-                Log.i("Thread Test","Hello Runnable!");
-            }
-        };
-
-        // 2.2.
-        new Thread(thread2).start();
-
-        // 3.2 Thread 실행 3
-        CustomThread thread3 = new CustomThread();
-        thread3.start();
-
-        // 4.2 Runnable 실행
-        Thread thread4 = new Thread(new CustomRunnable());
-        thread4.start();
+        findViewById(R.id.btnBasic).setOnClickListener(this);
+        findViewById(R.id.btnTest).setOnClickListener(this);
     }
-}
 
-// 3.1 Thread 생성
-class CustomThread extends Thread {
     @Override
-    public void run() {
-        Log.i("Thread Test","Hello Custom Thread!");
-    }
-}
+    public void onClick(View v) {
+        Intent intent = null;
+        switch(v.getId()){
+            case R.id.btnBasic:
+                intent = new Intent(this, ThreadBasicActivity.class);
+                break;
+            case R.id.btnTest:
+                intent = new Intent(this, TestActivity.class);
+                break;
+        }
+        startActivity(intent);
 
-// 4.1 Runnable 구현
-class CustomRunnable implements Runnable {
-    @Override
-    public void run() {
-        Log.i("Thread Test","Hello Custom Runnable!");
     }
 }
